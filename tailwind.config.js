b4c45a2ff7b94ff19170df9f3380d95835ac15cb
yogagalang <yogagalang609@gmail.com>
2023-06-24 10:41:52 +0700

    dropdownnavbar

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {
+      display: ['group-focus'],
+      opacity: ['group-focus'],
+      inset: ['group-focus']
+    },
+  },
+  plugins: [require("daisyui")],
+}
+
